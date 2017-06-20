62f715360432a109591df87b5cd210ddb98fdbbf
Ngo Hieu <Ngo Hieu>
2017-06-20 08:23:59 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\TheLoai;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('thu',function(){
+    $theloai = TheLoai::find(1);
+    foreach($theloai->loaitin as $loaitin){
+        echo $loaitin->Ten."<br>";
+    }
+});
+
+Route::get('thu2',function(){
+    return view('admin.theloai.danhsach');
+});
